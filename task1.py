1223b813b90b891e3f59d6eb84cabdd07d498de4
Nataliia Strokan <nataliia.strokan@gmail.com>
2026-02-04 16:24:31 +0200

    add task1 solution

---
+++ task1.py
@@ -0,0 +1,31 @@
+def total_salary(path: str) -> tuple:
+    """
+    Calculates total and average salary from a file.
+    Returns integers as required by the task.
+    """
+    try:
+        total = 0
+        count = 0
+
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+
+                _, salary = line.split(",") # splitting data by comma in each line
+                total += int(salary)
+                count += 1
+
+        if count == 0:
+            return (0, 0)
+
+        average = total // count   # ← to ensure average is an integer
+        return (total, average)
+
+    except FileNotFoundError:
+        return (0, 0)
+    except (ValueError, OSError):
+        return (0, 0)
+    
+
